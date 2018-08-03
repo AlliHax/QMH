c6e289384d5473a2edee57e8bdf335734d01e7e9
Alli <ahoughton004@gmail.com>
2018-08-03 13:03:20 -0700

    Added in Sound Effect for Duster

--- Quick! Mother is Home!/Assets/Scripts/Chore Sprites/MiniGame Managers/DustingMiniGameManager.cs
+++ Quick! Mother is Home!/Assets/Scripts/Chore Sprites/MiniGame Managers/DustingMiniGameManager.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class DustingMiniGameManager : MonoBehaviour {
     public GameObject duster;
     public int distance;
+    public AudioClip brushingSound;
+    public AudioSource dusterAudioSource;
     // Use this for initialization
 
     void Start()
     {
-
+        dusterAudioSource.clip = brushingSound;
     }
 
     // Update is called once per frame
     void OnMouseDrag()
     {
-
+        if (!dusterAudioSource.isPlaying)
+        {
+            dusterAudioSource.Play();
+        }
         duster.SetActive(true);
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
         duster.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
